6f7800d6c644c9db2998d1cd52ca0d5d40c1ffb5
Laurent Chau <mplaurent.chau@gmail.com>
2023-06-01 14:33:13 +0200

    refactor: harmonize middleware

--- apps/server/src/middlewares/class.ts
+++ apps/server/src/middlewares/class.ts
@@ -1,28 +1,49 @@
 import { body, param, query } from 'express-validator';
 
 export const classCreateValidator = [
-  body('name').exists().isString().notEmpty().withMessage('Name is required'),
-  body('teachers').optional().isArray(),
-  body('students').optional().isArray(),
+  body('name')
+    .exists({ checkFalsy: true })
+    .withMessage('Name is required')
+    .isString()
+    .withMessage('Name is required'),
+  body('teachers')
+    .optional()
+    .isArray()
+    .withMessage('Teachers must be an array'),
+  body('students')
+    .optional()
+    .isArray()
+    .withMessage('Students must be an array'),
 ];
 
-export const classGetValidator = [query('classId').optional().isString()];
+export const classGetValidator = [
+  query('classId')
+    .optional()
+    .isMongoId()
+    .withMessage('Class id must be a valid mongo id'),
+];
 
 export const classUpdateValidator = [
   param('classId')
-    .exists()
-    .isString()
-    .notEmpty()
+    .exists({ checkFalsy: true })
+    .withMessage('Class id is required')
+    .isMongoId()
     .withMessage('Class ID is required'),
-  body('name').optional().isString(),
-  body('teachers').optional().isArray(),
-  body('students').optional().isArray(),
+  body('name').optional().isString().withMessage('Name must be a string'),
+  body('teachers')
+    .optional()
+    .isArray()
+    .withMessage('Teachers must be an array'),
+  body('students')
+    .optional()
+    .isArray()
+    .withMessage('Students must be an array'),
 ];
 
 export const classDeleteValidator = [
   param('classId')
-    .exists()
-    .isString()
-    .notEmpty()
+    .exists({ checkFalsy: true })
+    .withMessage('Class id is required')
+    .isMongoId()
     .withMessage('Class ID is required'),
 ];
--- apps/server/src/middlewares/student.ts
+++ apps/server/src/middlewares/student.ts
@@ -1,31 +1,40 @@
 import { body, param, query } from 'express-validator';
 
 export const studentCreateValidator = [
-  body('name').exists().isString().notEmpty().withMessage('Name is required'),
+  body('name')
+    .exists({ checkFalsy: true })
+    .withMessage('Name is required')
+    .isString()
+    .withMessage('Name is required'),
   body('classId')
     .exists()
-    .isString()
+    .isMongoId()
     .notEmpty()
     .withMessage('Class is required'),
 ];
 
-export const studentGetValidator = [query('studentId').optional().isString()];
+export const studentGetValidator = [
+  query('studentId')
+    .optional()
+    .isMongoId()
+    .withMessage('Student id must be a valid mongo id'),
+];
 
 export const studentUpdateValidator = [
   param('studentId')
-    .exists()
-    .isString()
-    .notEmpty()
+    .exists({ checkFalsy: true })
+    .withMessage('Student ID is required')
+    .isMongoId()
     .withMessage('Student ID is required'),
-  body('name').optional().isString(),
-  body('classId').optional().isString(),
-  body('grade').optional().isObject(),
+  body('name').optional().isString().withMessage('Name must be a string'),
+  body('classId').optional().isMongoId().withMessage('Class is required'),
+  body('grade').optional().isObject().withMessage('Grade must be an object'),
 ];
 
 export const studentDeleteValidator = [
   param('studentId')
-    .exists()
-    .isString()
-    .notEmpty()
+    .exists({ checkFalsy: true })
+    .withMessage('Student ID is required')
+    .isMongoId()
     .withMessage('Student ID is required'),
 ];
